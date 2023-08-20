8bef6762c7e455b404c0eda4933e340e1357cb69
Harshit Srivastava <69175270+subsavage@users.noreply.github.com>
2023-08-20 15:57:57 +0530

    init

---
+++ src/main/java/com/subsavage/talktide/TalktideApplication.java
@@ -0,0 +1,13 @@
+package com.subsavage.talktide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalktideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalktideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/subsavage/talktide/TalktideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subsavage.talktide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalktideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
